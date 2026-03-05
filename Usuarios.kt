e96eef8f48c57124f848bb281b14aecf09017e26
Luna Angélica Giraldo Pulgarín <lunagp03@gmail.com>
2026-03-04 21:08:51 -0500

    Cambios en Usuario

--- Usuarios.kt
+++ Usuarios.kt
@@ -1,13 +1,28 @@
 class Usuario( val documento: String, val telefono: String, val correo: String) {
     fun validarDocumento(): Boolean {
-        return documento.isNotBlank()
+        if(documento.isNotBlank()) {
+            return true
+        } else {
+            println("\n --Documento vacío")
+            return documento.isNotBlank()
+        }
     }
 
     fun validarTelefono(): Boolean {
-        return telefono.all { it.isDigit() }
+        if(telefono.all { it.isDigit() }) {
+            return true
+        } else {
+            println("\n --Teléfono inválido")
+            return false
+        }
     }
 
     fun validarCorreo(): Boolean {
-        return correo.contains("@") && correo.contains(".")
+        if(correo.contains("@") && correo.contains(".")) {
+            return true
+        } else {
+            println("\n --Correo inválido")
+            return false
+        }
     }
 }
